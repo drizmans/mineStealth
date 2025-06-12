f02080e16aef3ddfcde1a99937aa33fcdc817025
Jack Jenkins <jackjenkins@penguin.local>
2025-06-12 20:34:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mineStealth
---
+++ src/main/java/drizmans/mineStealth/MineStealth.java
@@ -0,0 +1,17 @@
+package drizmans.mineStealth;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MineStealth extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
